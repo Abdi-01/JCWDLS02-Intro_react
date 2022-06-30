496a78fab6ccc14da9073a0686a8371c4f482af9
Abdi-01 <abdialghi@gmail.com>
2022-06-30 13:28:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import HomePage from './Pages/HomePage';
 
@@ -7,20 +6,6 @@ function App() {
   return (
     <div className="App">
       <HomePage/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to React Environtment
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,17 +7,51 @@ class HomePage extends React.Component {
 
         // Media penyimpanan utama pada setiap component react
         this.state = {
-
+            name: "",
+            counter: 0
         }
+    }
+
+    btnSetName = () => {
+        this.setState({ name: this.refs.inputName.value });
+    }
+
+    printName = () => {
+        return <h1>{this.state.name}</h1>
+    }
+
+    handleInput = (element) => {
+        console.log(element.target)
+    }
+
+    btnInc = () => {
+        this.setState({ counter: this.state.counter += 1 })
     }
 
 
     // Fungsi method untuk merender component html react nantinya
     render() {
-
+        let { name, counter } = this.state
         return (
-            <div>
-                <h1>Home Page</h1>
+            <div className="container">
+                <h1>Introduction State</h1>
+                <input type="text"
+                    placeholder="Input Name"
+                    className="form-control w-25 m-auto"
+                    ref="inputName"
+                    onChange={this.handleInput}
+                />
+                <button className="btn btn-primary my-2"
+                    type="button" onClick={this.btnSetName}>Set Name</button>
+                {this.printName()}
+                <div id="counter">
+                    <h1>Counter</h1>
+                    <div>
+                        <button type="button" className="btn btn-warning" onClick={() => this.setState({ counter: counter -= 1 })}>-</button>
+                        <span className="fw-bold mx-3">{counter}</span>
+                        <button type="button" className="btn btn-success" onClick={this.btnInc}>+</button>
+                    </div>
+                </div>
             </div>
         );
     }
