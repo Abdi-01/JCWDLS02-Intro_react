e3627bf6a0adb11fb53dbdb8910b23df68f19e68
Abdi-01 <abdialghi@gmail.com>
2022-06-30 09:30:24 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './Pages/HomePage';
 
+// functional component
 function App() {
   return (
     <div className="App">
+      <HomePage/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to React Environtment
         </p>
         <a
           className="App-link"
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+// Class Components
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // Media penyimpanan utama pada setiap component react
+        this.state = {
+
+        }
+    }
+
+
+    // Fungsi method untuk merender component html react nantinya
+    render() {
+
+        return (
+            <div>
+                <h1>Home Page</h1>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
