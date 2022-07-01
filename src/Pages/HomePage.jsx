7be19b65a8caaa52163182cbabbc4400819aeecd
Abdi-01 <abdialghi@gmail.com>
2022-07-01 09:41:09 +0700

    todo app

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -8,7 +8,9 @@ class HomePage extends React.Component {
         // Media penyimpanan utama pada setiap component react
         this.state = {
             name: "",
-            counter: 0
+            counter: 0,
+            data: [],
+            todo: ""
         }
     }
 
@@ -19,19 +21,31 @@ class HomePage extends React.Component {
     printName = () => {
         return <h1>{this.state.name}</h1>
     }
-
-    handleInput = (element) => {
-        console.log(element.target)
-    }
-
     btnInc = () => {
         this.setState({ counter: this.state.counter += 1 })
     }
 
 
+    handleInput = (element) => {
+        // console.log(element.target.value)
+        this.setState({ todo: element.target.value })
+    }
+
+    btnSubmit = () => {
+        let temp = [...this.state.data]; // menyalin data sebelumnya dari state.data
+        // Menambahkan data yg baru kedalam temp
+        temp.push({
+            id: this.state.data.length + 1,
+            todo: this.state.todo
+        });
+        this.setState({ data: temp });
+    }
+
+
+
     // Fungsi method untuk merender component html react nantinya
     render() {
-        let { name, counter } = this.state
+        let { name, counter, data } = this.state
         return (
             <div className="container">
                 <h1>Introduction State</h1>
@@ -39,7 +53,6 @@ class HomePage extends React.Component {
                     placeholder="Input Name"
                     className="form-control w-25 m-auto"
                     ref="inputName"
-                    onChange={this.handleInput}
                 />
                 <button className="btn btn-primary my-2"
                     type="button" onClick={this.btnSetName}>Set Name</button>
@@ -50,6 +63,16 @@ class HomePage extends React.Component {
                         <button type="button" className="btn btn-warning" onClick={() => this.setState({ counter: counter -= 1 })}>-</button>
                         <span className="fw-bold mx-3">{counter}</span>
                         <button type="button" className="btn btn-success" onClick={this.btnInc}>+</button>
+                    </div>
+                </div>
+                <div id="todo-app" className="card p-3 shadow w-75 m-auto my-5">
+                    <h1>To Do List</h1>
+                    <div className="input-group">
+                        <input type="text" className="form-control" onChange={this.handleInput} />
+                        <button className="btn btn-outline-primary" onClick={this.btnSubmit}>Create</button>
+                    </div>
+                    <div className="alert alert-primary my-2 shadow-sm">
+                        {data.length} Task
                     </div>
                 </div>
             </div>
