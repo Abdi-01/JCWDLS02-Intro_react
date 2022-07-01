4a57f52957ef2b8572cc1b77c1dbfce6e27b0eaf
Abdi-01 <abdialghi@gmail.com>
2022-07-01 09:47:48 +0700

    todo app

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,24 +7,11 @@ class HomePage extends React.Component {
 
         // Media penyimpanan utama pada setiap component react
         this.state = {
-            name: "",
             counter: 0,
             data: [],
             todo: ""
         }
     }
-
-    btnSetName = () => {
-        this.setState({ name: this.refs.inputName.value });
-    }
-
-    printName = () => {
-        return <h1>{this.state.name}</h1>
-    }
-    btnInc = () => {
-        this.setState({ counter: this.state.counter += 1 })
-    }
-
 
     handleInput = (element) => {
         // console.log(element.target.value)
@@ -45,26 +32,9 @@ class HomePage extends React.Component {
 
     // Fungsi method untuk merender component html react nantinya
     render() {
-        let { name, counter, data } = this.state
+        let { data } = this.state
         return (
             <div className="container">
-                <h1>Introduction State</h1>
-                <input type="text"
-                    placeholder="Input Name"
-                    className="form-control w-25 m-auto"
-                    ref="inputName"
-                />
-                <button className="btn btn-primary my-2"
-                    type="button" onClick={this.btnSetName}>Set Name</button>
-                {this.printName()}
-                <div id="counter">
-                    <h1>Counter</h1>
-                    <div>
-                        <button type="button" className="btn btn-warning" onClick={() => this.setState({ counter: counter -= 1 })}>-</button>
-                        <span className="fw-bold mx-3">{counter}</span>
-                        <button type="button" className="btn btn-success" onClick={this.btnInc}>+</button>
-                    </div>
-                </div>
                 <div id="todo-app" className="card p-3 shadow w-75 m-auto my-5">
                     <h1>To Do List</h1>
                     <div className="input-group">
